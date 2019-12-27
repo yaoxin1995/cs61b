2bba37e95a0c7ef9b89f4d07d264c2a61215610c
yaoxinjing <yaoxinjing517@gmail.com>
2019-12-27 09:24:29 +0100

    build the klette of the Array

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,35 @@
+public class ArrayDeque<T>{
+
+
+    public  ArrayDeque(){
+
+    }
+
+    public ArrayDeque(ArrayDeque other){}
+
+
+    /**
+     * Adds an item of type T to the front
+     * of the deque*/
+    public void addFirst(T item){
+
+    }
+    public void addLast(T item){
+
+    }
+    public boolean isEmpty(){}
+
+    public int size(){}
+
+    public void printDeque(){\
+    }
+
+    public T removeFirst(){}
+
+    public T removeLast(){}
+
+    public T get(int index){}
+
+
+
+}
